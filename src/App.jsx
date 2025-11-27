c8c3fe43b89e3c712acea5486919ed347c75fcad
Richard-collab <52125169+Richard-collab@users.noreply.github.com>
2025-11-27 19:56:50 +0800

    Update src/App.jsx with new App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from "react";
 import { useContext } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from "@vercel/analytics/react"
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <>
       <SpeedInsights/>
       <Analytics/>
+      <SearchBar />
       <ListBasicItemCard n={10} ascending={true}/>
     </>
   )
