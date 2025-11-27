5b723a045cfd801bb69bb9e425cad3705ebedd3b
Richard-collab <1982744285@qq.com>
2025-11-27 13:28:30 +0800

    add Analytics and SpeedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import ListBasicItemCard from "./components/ListBasicItemCard";
 import { useState } from "react";
 import { useContext } from "react";
-// import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
+      <SpeedInsights/>
+      <Analytics/>
       <ListBasicItemCard n={10} ascending={true}/>
     </>
   )
