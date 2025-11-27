50434725a91e1b35b6acf1a7da41f91b8393351a
Richard-collab <52125169+Richard-collab@users.noreply.github.com>
2025-11-27 20:20:45 +0800

    Update src/App.jsx with new content

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import ListBasicItemCard from "./components/ListBasicItemCard";
 import { useState } from "react";
-import { useContext } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Analytics } from "@vercel/analytics/react"
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const [searchKeyword, setSearchKeyword] = useState('');
   return (
     <>
       <SpeedInsights/>
       <Analytics/>
-      <SearchBar />
-      <ListBasicItemCard n={10} ascending={true}/>
+      <SearchBar onSearch={kw => setSearchKeyword(kw)} />
+      <ListBasicItemCard n={10} ascending={true} keyword={searchKeyword} />
     </>
   )
 }
 
-export default App
+export default App;
